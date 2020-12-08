b012a41f072adfc0087265626d973351b5d605d1
Sergey Vorontsov <aurum1997@gmail.com>
2020-12-08 18:33:54 +0300

    Добавление layout

--- bin/general/correct_address.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-ROOT_SNAP=$(dirname "$(readlink -f "$0")")/..
-DIR_OF_LOCAL="$ROOT_SNAP"/usr/share/X11/locale
-for var in $(find "$DIR_OF_LOCAL" -type f -name "Compose")
-do
-$(sudo sed -i 's/\"\/usr/\"\/snap\/dalrao-app\/current\/usr/' "$var")
-done
