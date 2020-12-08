c0365a0bf8b18955917a4bc4f267d2d30c3262e7
Sergey Vorontsov <aurum1997@gmail.com>
2020-12-08 17:50:32 +0300

    Добавил "sudo"

--- bin/general/correct_address.sh
+++ bin/general/correct_address.sh
@@ -3,5 +3,5 @@ ROOT_SNAP=$(dirname "$(readlink -f "$0")")/..
 DIR_OF_LOCAL="$ROOT_SNAP"/usr/share/X11/locale
 for var in $(find "$DIR_OF_LOCAL" -type f -name "Compose")
 do
-$(sed -i 's/\"\/usr/\"\/snap\/dalrao-app\/current\/usr/' "$var")
+$(sudo sed -i 's/\"\/usr/\"\/snap\/dalrao-app\/current\/usr/' "$var")
 done
